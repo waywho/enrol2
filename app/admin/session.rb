d6a25d35a9a597b75362de1f125986d6a6f19f4a
WeiHsi <weihsi.hu@gmail.com>
2015-06-06 20:08:42 +0000

    up to new cart scaffold

---
+++ app/admin/session.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Session do
+	index do
+		column :id
+		column :date
+		column :from
+		column :to
+		column :instructor
+		column :address
+		column :postal_code
+		column :cost
+		column :user_id
+		default_actions
+	end
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+permit_params :date, :from, :to, :instructor, :course_id, :address, :postal_code, :latitude, :longitude, :cost
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605212522) do
+ActiveRecord::Schema.define(version: 20150606200222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,14 @@ ActiveRecord::Schema.define(version: 20150605212522) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "courses", force: true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.decimal  "cost"
     t.integer  "user_id"
     t.datetime "created_at"

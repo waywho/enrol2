9c7162df575bd1adeb0ca8d21807e9347b6872b2
WeiHsi Hu <weihsi.hu@gmail.com>
2015-07-14 21:42:47 +0000

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'enrolnow.herokuapp.com',
+    :domain         => 'enrolnow.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

09301ee6cd70579088c52fb4b0cfbc02f7b06aa9
WeiHsi Hu <weihsi.hu@gmail.com>
2016-03-21 23:37:17 +0000

    session correct

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,8 +4,8 @@ module CurrentCart
 	private
 
 	def set_cart
-		if session[:cart_id]
-			@cart ||= Cart.find(session[:cart_id])
+		if session[:cart_id].present?
+			@cart = Cart.find(session[:cart_id])
 			session[:cart_id] = nil if @cart.purchased_at
 		end
 		if session[:cart_id].nil?
